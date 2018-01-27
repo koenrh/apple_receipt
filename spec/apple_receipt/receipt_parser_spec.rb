47326ce78fa1dbd602aed7b75e3103b7ad1337b7
Koen Rouwhorst <info@koenrouwhorst.nl>
2018-01-27 17:28:15 +0100

    Added support for receipt version 2.

--- spec/apple_receipt/receipt_parser_spec.rb
+++ spec/apple_receipt/receipt_parser_spec.rb
@@ -6,7 +6,35 @@ require 'apple_receipt/receipt_parser'
 
 describe AppleReceipt::ReceiptParser do
   describe '.parse' do
-    it 'parses the receipt' do
+    it 'parses a raw receipt' do
+      signature = Base64.strict_encode64('abc')
+      purchase_info = Base64.strict_encode64('baz')
+      data = "{\n\t\"signature\" = \"#{signature}\";\n"\
+        "\t\"purchase-info\" = \"#{purchase_info}\";\n}"
+
+      AppleReceipt::ReceiptParser.expects(:read_signature)
+                                 .with('abc')
+                                 .returns([1, 2, 3])
+      receipt_data = AppleReceipt::ReceiptParser.parse(data)
+      receipt_data.must_equal([1, 2, 3, 'baz'])
+    end
+
+    it 'raises an error when a required field is missing' do
+      err = lambda do
+        AppleReceipt::ReceiptParser.parse('{}')
+      end.must_raise ArgumentError
+      err.message.must_equal 'Missing required fields'
+    end
+
+    it 'raises an error for unsupported versions' do
+      test = [4].pack('C')
+      err = lambda do
+        AppleReceipt::ReceiptParser.read_signature(test)
+      end.must_raise ArgumentError
+      err.message.must_equal 'Unsupported receipt version: 4'
+    end
+
+    it 'parses a valid receipt' do
       valid_receipt_raw = File.read('./spec/fixtures/valid_receipt.txt')
       valid_receipt = Base64.decode64(valid_receipt_raw)
       v, size, cert, data = AppleReceipt::ReceiptParser.parse(valid_receipt)

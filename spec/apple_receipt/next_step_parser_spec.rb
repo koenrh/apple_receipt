ac6bcf623ee023ffca04c7012f4c15d21eb6e7fc
Koen Rouwhorst <info@koenrouwhorst.nl>
2018-01-27 15:36:16 +0100

    Removed comments.

--- spec/apple_receipt/next_step_parser_spec.rb
+++ spec/apple_receipt/next_step_parser_spec.rb
@@ -7,8 +7,6 @@ require 'apple_receipt/next_step_parser'
 describe AppleReceipt::NextStepParser do
   describe '.parse' do
     it 'transforms nextstep data into a hash' do
-      # sig_b64 = Base64.encode64('abc').delete("\n")
-      # data_b64 = Base64.encode64('123').delete("\n")
       nextstep = "{\n"\
         "\t\"foo\" = \"abc\";\n"\
         "\t\"bar\" = \"baz\";\n}"

64847a044fc8d314567a343ba182f38a15670e94
Koen Rouwhorst <info@koenrouwhorst.nl>
2018-01-27 14:57:35 +0100

    Fixed RuboCop offences.

--- lib/apple_receipt.rb
+++ lib/apple_receipt.rb
@@ -1,5 +1,8 @@
-require "apple_receipt/version"
+# frozen_string_literal: true
 
+require 'apple_receipt/version'
+
+# AppleReceipt is the main module
 module AppleReceipt
   # Your code goes here...
 end

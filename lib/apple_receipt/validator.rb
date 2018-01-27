7599903cfeb20a6a0bcc99df08011598bfed47c6
Koen Rouwhorst <info@koenrouwhorst.nl>
2018-01-27 17:53:43 +0100

    Loaded proper intermediate certificate.

--- lib/apple_receipt/validator.rb
+++ lib/apple_receipt/validator.rb
@@ -5,17 +5,37 @@ require 'openssl'
 module AppleReceipt
   # Validator allows one to check the validity of a receipt.
   class Validator
-    def initialize(receipt)
-      root_cert_pem = File.read('./certificates/AppleIncRootCertificate.cer')
-      root_cert = OpenSSL::X509::Certificate.new(root_cert_pem)
-
-      intermediate_cert_pem = File.read('./certificates/AppleWWDRCA.cer')
-      intermediate_cert = OpenSSL::X509::Certificate.new(intermediate_cert_pem)
-
-      store.add_cert(root_cert)
-      store.add_cert(intermediate_cert)
-
+    def initialize(receipt, certificates: [])
+      populate_certificate_store(receipt.version, certificates)
       @receipt = receipt
+    end
+
+    def populate_certificate_store(version, provided_certificates)
+      certificates = if provided_certificates.any?
+                       provided_certificates
+                     else
+                       default_chain(version)
+                     end
+      add_certificates(certificates)
+    end
+
+    def default_chain(version)
+      root_cert_pem = File.read('./certificates/AppleIncRootCertificate.cer')
+      intermediate_cert_pem = case version
+                              when 3
+                                File.read('./certificates/AppleWWDRCA.cer')
+                              when 2
+                                File.read('./certificates/AppleISCA.cer')
+                              end
+
+      [OpenSSL::X509::Certificate.new(root_cert_pem),
+       OpenSSL::X509::Certificate.new(intermediate_cert_pem)]
+    end
+
+    def add_certificates(certificates)
+      certificates.each do |cert|
+        store.add_cert(cert)
+      end
     end
 
     def valid?
